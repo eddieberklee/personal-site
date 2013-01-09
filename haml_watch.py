78f8dc60a00bf7ad327839b209ad428943f79949
Eddie Lee <eddieberklee@gmail.com>
2013-01-09 04:14:26 -0800

    added a skills section

--- haml_watch.py
+++ haml_watch.py
@@ -3,17 +3,21 @@
 
 import sys, os, time
 
-file_complete = raw_input('enter file name (*.haml): ')
-filename = file_complete[:file_complete.find('.')]
+# file_complete = raw_input('enter file name (*.haml): ')
+filename_complete = sys.argv[1]
+filename = filename_complete[:filename_complete.find('.')]
 haml_call = "haml " + filename + ".haml " + filename + ".html"
 print haml_call
 os.system(haml_call)
-old_call = os.stat(file_complete)
+old_call = os.stat(filename_complete)
 while 1:
   time.sleep(0.9)
-  current_call = os.stat(file_complete)
+  try:
+    current_call = os.stat(filename_complete)
+  except:
+    pass
   if current_call != old_call:
-    print 'Detected a change in ' + file_complete
+    print 'Detected a change in ' + filename_complete
     print haml_call
     os.system(haml_call)
     old_call = current_call
